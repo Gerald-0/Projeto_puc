977926806656fbc4c51bc532163dea14186354d1
Gerald Santos <79993851+Gerald-0@users.noreply.github.com>
2021-11-25 18:29:33 -0300

    Update prestador.php

--- php/prestador.php
+++ php/prestador.php
@@ -16,15 +16,16 @@ $senha=    $_SESSION['senha'];
 $nome_comercio=$_POST['nome_do_comercio'];
 $servico_comercio=$_POST['servico_do_comercio'];
 $cnpj=$_POST['cnpj'];
+$servico=$_POST['valor_servico'];
 
 
-
-$sql = "INSERT INTO tbprestador  (nome,data_nascimento,cpf,email,nome_do_comercio,servico_do_comercio,cnpj)
-VALUES ('" .  $nome . "','".$data_nascimento . "','" .  $cpf . "','" . $email . "','".   $nome_comercio . "','".$servico_comercio . "','" .  $cnpj . "')"; 
+$sql = "INSERT INTO tbprestador  (senha,nome,data_nascimento,cpf,email,nome_do_comercio,servico_do_comercio,cnpj,valor_servico) 
+VALUES ('$senha','$nome','$data_nascimento','$cpf','$email','$nome_comercio','$servico_comercio','$cnpj','$servico')";
 
 $sql = $conn->prepare($sql);
 $sql->execute();
 $result = $sql;
+
 
 
 
